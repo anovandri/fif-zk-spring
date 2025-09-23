a1b7658056e33efd6be4f3c80183106ddc8c1096
Aditya Novandri <aditya.novandri@tech.jago.com>
2025-09-23 20:47:05 +0700

    chore: change database connection from hsqldb to postgresql

--- src/main/java/com/fif/acl/entity/User.java
+++ src/main/java/com/fif/acl/entity/User.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Table(name = "\"user\"")
 public class User {
 
   @Id
--- src/main/resources/db/migration/V2__Insert_initial_data.sql
+++ src/main/resources/db/migration/V2__Insert_initial_data.sql
@@ -1,10 +1,13 @@
--- V1__Insert_initial_data.sql --
-INSERT INTO role (id, name) VALUES (1, 'ROLE_USER');
-INSERT INTO role (id, name) VALUES (2, 'ROLE_ADMIN');
+-- V2__Insert_initial_data.sql
+-- Insert roles
+INSERT INTO "role" (id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO "role" (id, name) VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO user (id, username, password) VALUES (1, 'admin', '$2a$10$oRm5ddTjHMhNsi.NVKRLQOiUXz6qs1zPUhkZGd5TM/7Y0F/MLkBae');
-INSERT INTO user (id, username, password) VALUES (2, 'user', '$2a$10$4koVDNa1I8P6u2LqN4fx5e1hSgdkabthueqrWpTJGi9GFIIDUD4AC');
+-- Insert users (note: 'user' is quoted because it's a reserved keyword in PostgreSQL)
+INSERT INTO "user" (id, username, password) VALUES (1, 'admin', '$2a$10$oRm5ddTjHMhNsi.NVKRLQOiUXz6qs1zPUhkZGd5TM/7Y0F/MLkBae');
+INSERT INTO "user" (id, username, password) VALUES (2, 'user', '$2a$10$4koVDNa1I8P6u2LqN4fx5e1hSgdkabthueqrWpTJGi9GFIIDUD4AC');
 
+-- Insert user-role relationships
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 2);
 INSERT INTO user_roles (user_id, role_id) VALUES (2, 1);
