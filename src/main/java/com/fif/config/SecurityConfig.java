6a85f1cc90caaa60a22ed288ffdf76da4daa76f1
Aditya Novandri <aditya.novandri@tech.jago.com>
2025-09-24 09:02:38 +0700

    chore: allow ROLE_USER to access to index.zul

--- src/main/java/com/fif/config/SecurityConfig.java
+++ src/main/java/com/fif/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.csrf().disable()
         .authorizeRequests()
         .antMatchers("/index.zul")
-        .hasRole("ADMIN")
+        .hasAnyRole("ADMIN", "USER")
         .and()
         .formLogin()
         .loginPage("/login.zul")
